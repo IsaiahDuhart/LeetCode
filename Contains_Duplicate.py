2ab1351d4e3052e5bea183c833f43ea9f4d91ea7
IsaiahDuhart <isaiahd@vt.edu>
2023-08-02 23:03:29 -0400

    Contains Duplicate 8/2/2023
    :q

---
+++ Contains_Duplicate.py
@@ -0,0 +1,11 @@
+from ast import List
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        dic ={}
+        for x in nums:
+            if x in dic:
+                return True
+            else:
+                dic[x] = x
+        return False
